47e7b1bfdd261514878cabde9d5690e1240e5272
pranay <pranay4saini@gmail.com>
2022-11-09 14:19:22 +0530

    finishing up layout design

---
+++ app/src/main/java/com/pranay/anydo/Activity/InsertNotesActivity.java
@@ -0,0 +1,16 @@
+package com.pranay.anydo.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pranay.anydo.R;
+
+public class InsertNotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_notes);
+    }
+}
---
+++ app/src/main/java/com/pranay/anydo/Activity/UpdateNotesActivity.java
@@ -0,0 +1,16 @@
+package com.pranay.anydo.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pranay.anydo.R;
+
+public class UpdateNotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_notes);
+    }
+}
