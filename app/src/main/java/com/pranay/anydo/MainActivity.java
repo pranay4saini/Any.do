97c0321d3e83dd9743589c84889da6ca32de2f6f
pranay <pranay4saini@gmail.com>
2022-11-11 02:48:00 +0530

    added notes filtering UI

--- app/src/main/java/com/pranay/anydo/MainActivity.java
+++ app/src/main/java/com/pranay/anydo/MainActivity.java
@@ -2,6 +2,7 @@ package com.pranay.anydo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
@@ -14,27 +15,50 @@ import com.pranay.anydo.Adapter.NotesAdapter;
 import com.pranay.anydo.ViewModel.NotesViewModel;
 
 
-
 public class MainActivity extends AppCompatActivity {
 
     private NotesViewModel notesViewModel;
 
-   private  RecyclerView notesRecycler;
-   private NotesAdapter adapter;
-   private FloatingActionButton addNewNotes;
+    private RecyclerView notesRecycler;
+    private NotesAdapter adapter;
+    private FloatingActionButton addNewNotes;
+    TextView nofilter, hightolow, lowtohigh;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         notesRecycler = findViewById(R.id.notes_recyclerview);
         addNewNotes = findViewById(R.id.add_notes_fab);
+
         notesViewModel = ViewModelProviders.of(this).get(NotesViewModel.class);
 
+        nofilter = findViewById(R.id.noFilter);
+        hightolow = findViewById(R.id.highToLow);
+        lowtohigh = findViewById(R.id.lowToHigh);
+
+        nofilter.setBackgroundResource(R.drawable.filter_selected_shape);
+
+        nofilter.setOnClickListener(v -> {
+            hightolow.setBackgroundResource(R.drawable.filter_unselected_shape);
+            lowtohigh.setBackgroundResource(R.drawable.filter_unselected_shape);
+            nofilter.setBackgroundResource(R.drawable.filter_selected_shape);
+        });
+        hightolow.setOnClickListener(v -> {
+            hightolow.setBackgroundResource(R.drawable.filter_selected_shape);
+            lowtohigh.setBackgroundResource(R.drawable.filter_unselected_shape);
+            nofilter.setBackgroundResource(R.drawable.filter_unselected_shape);
+        });
+        lowtohigh.setOnClickListener(v -> {
+            hightolow.setBackgroundResource(R.drawable.filter_unselected_shape);
+            lowtohigh.setBackgroundResource(R.drawable.filter_selected_shape);
+            nofilter.setBackgroundResource(R.drawable.filter_unselected_shape);
+        });
         addNewNotes.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, InsertNotesActivity.class)));
         notesViewModel.getallNotes.observe(this, notes -> {
 
-            notesRecycler.setLayoutManager(new GridLayoutManager(this,2));
-            adapter = new NotesAdapter(MainActivity.this,notes);
+            notesRecycler.setLayoutManager(new GridLayoutManager(this, 2));
+            adapter = new NotesAdapter(MainActivity.this, notes);
             notesRecycler.setAdapter(adapter);
 
         });
